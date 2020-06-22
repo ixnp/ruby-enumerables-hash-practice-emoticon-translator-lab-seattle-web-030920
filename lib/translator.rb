9885b8061e0c12ee66405d39d11e157d44a59719
Ix Procopios <ix.procopios@flatironschool.com>
2020-06-22 20:58:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,29 @@
-# require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
+  
+  emoticon_hash = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
+  
+  emoticons.each do |key, value|
+    puts 'keys: '
+    puts key
+    puts 'values: '
+    puts value
+    puts '    '
+  end 
+  
+  emoticon_hash
+
 end
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
 def get_english_meaning
-  # code goes here
+
 end
